a5c49d901cf312e433f101c3b3504b060629be98
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-08-17 17:49:49 +0200

    28th - on branch 1.2_dev - connecting loader on submit button

--- GrandPyBot/static/js/loading.js
+++ GrandPyBot/static/js/loading.js
@@ -1,12 +1,53 @@
-function maskContent()
-{
-    document.querySelector(".loader-container").style.opacity = 0;
+    function maskContent()
+    {
+        document.querySelector(".loader-container").style.opacity = 0;
 
-}
-function showContent()
-{
-    document.querySelector(".loader-container").style.opacity = 1;setTimeout(maskContent, 3000);
+    }
+    function showContent()
+    {
+        document.querySelector(".loader-container").style.opacity = 1;setTimeout(maskContent, 3000);
 
-}
-setTimeout(showContent, 3000);
+    }
+form_sheet.addEventListener("submit", function(event)
+{
+    event.preventDefault();
+    showContent();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////form_sheet.addEventListener("submit", function(event)
+////{
+////    event.preventDefault();
+////    postFormData("/catcher", new FormData(form_sheet))
+////    .then(
+//    function maskContent()
+//    {
+//        document.querySelector(".loader-container").style.opacity = 0;
+//
+//    }
+//    function showContent()
+//    {
+//        document.querySelector(".loader-container").style.opacity = 1;setTimeout(maskContent, 3000);
+//
+//    }
+//    //setTimeout(showContent, 5000);
+//    showContent();
+////    )
+////})
 
