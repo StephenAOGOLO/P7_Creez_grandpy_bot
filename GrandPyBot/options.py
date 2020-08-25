fb1760ff5e3f2fb69128d0ac65421b709666d165
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-08-26 01:18:35 +0200

    changing ini pathfile

--- GrandPyBot/options.py
+++ GrandPyBot/options.py
@@ -17,7 +17,7 @@ class Settings:
         This constructor create a instance
         which contains all the data from a file ini.
         """
-        self.file_ini = ".\\GrandPyBot\\static\\ini\\settings.ini"
+        self.file_ini = ".\\app\\GrandPyBot\\static\\ini\\settings.ini"
         self.all_sections_file = self.get_all_sections_file_ini()
 
     def get_data_file_ini(self, section):
