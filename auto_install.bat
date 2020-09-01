0224867be0ebc25fb05fca731fe9d42a7f867746
StephenAOGOLO <56355715+StephenAOGOLO@users.noreply.github.com>
2020-09-02 01:38:23 +0200

    Delete auto_install.bat

--- auto_install.bat
+++
@@ -1,7 +0,0 @@
-SET HOME=%CD%
-cd ..
-python -m virtualenv -p python %HOME%
-cd %HOME%
-cd .\Scripts
-cmd /k "activate&cd %HOME%&pip install -r requirements.txt&echo #&echo ### PREINSTALLATION TERMINE ###&echo #"
-pause
