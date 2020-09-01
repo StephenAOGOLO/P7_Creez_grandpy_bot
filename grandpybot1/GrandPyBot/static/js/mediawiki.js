a2633c4d42a3d299ed6368a05b97619d7088f9ef
StephenAOGOLO <56355715+StephenAOGOLO@users.noreply.github.com>
2020-09-02 01:35:12 +0200

    Delete mediawiki.js

--- grandpybot1/GrandPyBot/static/js/mediawiki.js
+++
@@ -1,7 +0,0 @@
-form_sheet.addEventListener("submit", function(event)
-{
-    event.preventDefault();
-    postFormData("/catcher", new FormData(form_sheet))
-    .then(response => {console.log(response);document.querySelector("#response").textContent=response["article"];})
-})
-
