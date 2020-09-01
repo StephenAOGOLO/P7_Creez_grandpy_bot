994a5c9eb33857533f06a38a95db0503c66d0180
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2020-09-02 01:13:18 +0200

    50th - on branch 1.4_test - final version 1.4 - 2nd presentation

--- GrandPyBot/static/js/loading.js
+++ GrandPyBot/static/js/loading.js
@@ -1,26 +1,31 @@
+// Following functions handle the loading icon behavior during searching phases.
+    //This function hiding the website content.
     function maskContent()
     {
         document.querySelector(".loader-container").style.opacity = 0;
         setTimeout(stopLoading, 1000);
-
     }
+    //This function showing the website content.
     function showContent()
     {
         startLoading;
         document.querySelector(".loader-container").style.opacity = 1;setTimeout(maskContent, 4000);
     }
+    //This function makes the loading icon disappear.
     function stopLoading()
     {
         document.querySelector(".loader-container").style.height = '0%';
         document.querySelector(".loader-container").style.width = '0%';
         document.querySelector(".loader-container").style.top = '100%';
     }
+    //This function makes the loading icon appear.
     function startLoading()
     {
-        document.querySelector(".loader-container").style.height = '90%';
+        document.querySelector(".loader-container").style.height = '100%';
         document.querySelector(".loader-container").style.width = '100%';
         document.querySelector(".loader-container").style.top = '30%';
     }
+    //This function is a tempo.
     function justWait()
     {
         console.log("wait");
@@ -32,4 +37,3 @@ form_sheet.addEventListener("submit", function(event)
     showContent();
 
 })
-
