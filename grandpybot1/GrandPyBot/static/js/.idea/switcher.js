32adfd0d6149c34b2581a645253d1eb34d095020
StephenAOGOLO <56355715+StephenAOGOLO@users.noreply.github.com>
2020-09-02 01:35:51 +0200

    Delete switcher.js

--- grandpybot1/GrandPyBot/static/js/.idea/switcher.js
+++
@@ -1,3 +0,0 @@
-var content = document.getElementById("response").innerHTML;
-var new_content = document.getElementById("loader").innerHTML;
-document.getElementById("response").innerHTML = new_content;
